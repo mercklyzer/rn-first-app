b48a8dff1a01b93b2eb9aff0caa8f6edb16c820b
mercklyzer <mercklyzer@gmail.com>
2021-12-01 16:26:29 +0800

    end tutorial

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import GoalInput from './components/GoalInput';
+
+import GoalItem from './components/GoalItem';
+
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([])
+  const [isAddMode, setIsAddMode] = useState(false)
+
+  const addGoalHandler = (goalTitle) => {
+    setCourseGoals(currentGoals => [...currentGoals, {key: Math.random().toString(),value:goalTitle}])
+    setIsAddMode(false)
+  }
+
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals(currentGoals => currentGoals.filter((goal) => goal.key !== goalId))
+  }
+
+  const cancelGoalHandler = () => {
+    setIsAddMode(false)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)}/>
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalHandler}/>
+
+      {/* <ScrollView>
+        {courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
+      </ScrollView> */}
+      <FlatList 
+        data={courseGoals} 
+        renderItem={itemData =>  <GoalItem onDelete={removeGoalHandler} id={itemData.item.key} title={itemData.item.value}></GoalItem>}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
 });
---
+++ components/GoalItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const GoalItem = (props) => {
+    return (
+        // can use TouchableWithoutFeedback
+        <TouchableOpacity activeOpacity={0.8} onPress={props.onDelete.bind(this, props.id)}>
+            <View style={styles.listItem}>
+                <Text>{props.title}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    listItem: {
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: '#ccc',
+        borderColor: 'black',
+        borderWidth: 1
+    }
+})
+
+export default GoalItem
